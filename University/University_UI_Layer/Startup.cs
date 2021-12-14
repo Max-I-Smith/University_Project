5b2cef7272f53d02a933da274d20157befb8d572
Max-I-Smith <maxsmith3169@gmail.com>
2021-12-14 15:15:25 -0600

    Initial Commit

---
+++ University/University_UI_Layer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(University_UI_Layer.Startup))]
+namespace University_UI_Layer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
